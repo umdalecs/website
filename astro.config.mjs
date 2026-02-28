58f258eb2c9691718052d662e868089e5864c24e
Alejandro Flores <dev.alejandro.flores@gmail.com>
2026-02-28 13:06:13 -0700

    fix: build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig, passthroughImageService } from "astro/config";
+export default defineConfig({
+ image: {
+   service: passthroughImageService(),
+ },
+});
