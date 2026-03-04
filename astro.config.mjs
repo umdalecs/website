d668ef8d7e0bd6e24e5eae044d4645e6e0142565
Alejandro Flores <dev.alejandro.flores@gmail.com>
2026-03-03 21:40:47 -0700

    feat: skills section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+import icon from "astro-icon";
+
+import logos from "@iconify-json/logos/icons.json";
+import lucide from "@iconify-json/lucide/icons.json";
+import devicon from "@iconify-json/devicon/icons.json";
 
 export default defineConfig({
- image: {
-   service: passthroughImageService(),
- },
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    icon({
+      collections: {
+        logos,
+        lucide,
+        devicon,
+      },
+    }),
+  ],
 });
